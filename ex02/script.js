2c40cdfc8d15145a30b1b2befeebe986cb84c369
claravitoria <limaclarinha17@gmail.com>
2026-04-01 08:49:45 -0300

    Verificando se é maior de idade

---
+++ ex02/script.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("Insira a sua idade, por gentileza:"));
+
+if (idade >= 18){
+    alert("Você é maior de idade");
+} 
+else if (idade < 18) {
+    alert ("Você é menor de idade")
+}
