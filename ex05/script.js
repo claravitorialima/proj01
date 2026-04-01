11c24172a7ee6bf029f4db63cb7a4fdf9ab7fb54
claravitoria <limaclarinha17@gmail.com>
2026-04-01 09:44:51 -0300

    Número múltiplo de cinco

---
+++ ex05/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Insira um número, por favor:"));
+
+if (numero % 5 === 0){
+    alert("O número " + numero + " é múltiplo de 5");
+} else {
+    alert ("O número " + numero + " não é múltiplo de 5");
+}
