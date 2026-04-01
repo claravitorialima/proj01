1c4294d10a0d9fa355f0ddb5ad140cc181b58768
claravitoria <limaclarinha17@gmail.com>
2026-04-01 08:38:59 -0300

    Número maior ou menor que zero

---
+++ ex01/script.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Adicione, por favor um número:"));
+
+if (numero > 0) {
+    alert("Número Positivo");
+} else if (numero < 0) {
+    alert("Número Negativo");
+} else {
+    alert("O número é zero");
+}
